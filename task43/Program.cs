5f6ef6144b33900701ce0a81699678550e105d23
polina993 <>
2022-06-30 08:05:31 +0300

    add task43

---
+++ task43/Program.cs
@@ -0,0 +1,15 @@
+﻿double Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
+double k1 = Input("Введите k1: ");
+double k2 = Input("Введите k2: ");
+double b1 = Input("Введите b1: ");
+double b2 = Input("Введите b2: ");
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"х={x},y= {y}");
+
+
